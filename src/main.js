9f8ec2d4ec803c31f2b6b3ba92401c92876dffbc
jesuisundev <admin@jesuisundev.com>
2021-09-03 19:49:39 -0400

    resolution resolver + final url

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import Library from './world/Library'
 import Parameters from './world/Parameters'
 import PostProcessor from './postprocessing/PostProcessor'
 import Sequencer from './sequencer/sequencer'
+import Helper from './world/Helper'
 
 // ROAD MAP
 // TODO : handle mobile control - wip 
@@ -22,14 +23,16 @@ import Sequencer from './sequencer/sequencer'
 
 const clock = new THREE.Clock()
 const parameters = new Parameters()
+const helper = new Helper(parameters)
 
-setDefaultGlobal()
+helper.setDefaultGlobal()
 
 const scene = new THREE.Scene()
 scene.fog = new THREE.Fog(parameters.global.background[window.currentUniverse], parameters.global.camera.near, parameters.global.camera.far)
 
-const renderWidth = Math.floor(window.innerWidth / 1.2)
-const renderHeight = Math.floor(window.innerHeight / 1.2)
+const renderResolution = helper.getRenderResolution()
+const renderWidth = renderResolution.renderWidth
+const renderHeight = renderResolution.renderHeight
 const renderer = new THREE.WebGLRenderer(parameters.global.webGlRenderer)
 renderer.setClearColor(new THREE.Color(parameters.global.background[window.currentUniverse]))
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -124,10 +127,6 @@ function animate () {
   }
 }
 
-function isMobileOrTablet() {
-  return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test((navigator.userAgent||navigator.vendor||window.opera))||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test((navigator.userAgent||navigator.vendor||window.opera).substr(0,4))
-}
-
 /**
  * Handle preload of assets and show launch call to action
  */
@@ -143,16 +142,6 @@ function isMobileOrTablet() {
   await controls.showElementById("description")
   await controls.showElementById("notice")
   await controls.showElementById("entrypoint")
-}
-
-// i'm lazy, we could refactor some code and avoid globals
-function setDefaultGlobal() {
-  window.currentUniverse = 0
-  window.materialsToUpdate = {}
-  window.meshesToUpdate = {}
-  window.wormhole = { shape: null, CameraPositionIndex: 0, speed: parameters.wormhole.speed, active: false }
-  window.sequencer = { active: false }
-  window.isMobileOrTabletFlag = isMobileOrTablet()
 }
 
 function updateAnimatedObjects (elapsedTime) {
@@ -200,3 +189,5 @@ function updatePositionInWormhole () {
 
 animate()
 init()
+
+console.log('Ho hi fellow developer ! You\'ll find the source code over here => https://github.com/jesuisundev/acrossthemultiverse.')
